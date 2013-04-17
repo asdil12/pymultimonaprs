ce2071f3a5649f620d16b0262f935c6a9c2c594c
Dominik Heidler <dheidler@gmail.com>
2013-04-17 11:01:10 +0200

    send weather date
    - fixes #3

--- README.md
+++ README.md
@@ -19,9 +19,45 @@ Configuration
 -------------
 
 Edit `/etc/pymultimonaprs.json`:
-- Set the source to `rtl`, `alsa`, or `pulse` to select the backend
-- Set the status text, or set a status file - the content of this file will be read at runtime and sent as status.
-  This way you can eg. monitor your battery status using APRS-IG
+
+### Backend
+
+Set the source to `rtl`, `alsa`, or `pulse` to select the backend
+
+### Status
+
+Set the status `text`, or set a status `file` - the content of this file will be read at runtime and sent as status.
+This way you can eg. monitor your battery status using APRS-IG.
+Set both `text` and `file` to `false` to disable status beacon.
+
+### Weather
+
+You can set `weather` to a json-file. This will be read in like the status-file and can look like that:
+
+```json
+{
+	"timestamp": 1366148418,
+	"wind": {
+		"speed": 10,
+		"direction": 240,
+		"gust": 200
+	},
+	"temperature": 18,
+	"humidity": 20,
+	"pressure": 1013.25
+}
+```
+
+#### Legend
+
+- `timestamp` is seconds since epoch - **must** be included
+- `wind`
+	- `speed` is in km/h
+	- `direction` is in deg
+	- `gust` is in deg
+- `temperature` is in °C
+- `humidity` is in %
+- `pressure` is in hPa
 
 Running
 -------
--- pymultimonaprs/beacon.py
+++ pymultimonaprs/beacon.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python2
 
 import os
+import json
+from datetime import datetime
 
 def encode_lat(lat):
 	lat_dir = 'N' if lat > 0 else 'S'
@@ -29,7 +31,57 @@ def get_status_frame(callsign, status):
 			status_text = status['text']
 		else:
 			return None
-		payload = ">%s" % (status_text)
+		payload = ">%s" % status_text
+		return "%s>APRS,TCPIP*:%s" % (callsign, payload)
+	except:
+		return None
+
+def get_weather_frame(callsign, weather):
+	try:
+		w = json.load(open(weather))
+
+		# Convert to imperial and encode
+
+		# Timestamp
+		wenc = datetime.utcfromtimestamp(int(w['timestamp'])).strftime('%m%d%H%M')
+
+		# Wind
+		wind = w.get('wind', {})
+		if 'direction' in wind:
+			wenc += "c%03d" % wind['direction']
+		else:
+			wenc += "c..."
+		if 'speed' in wind:
+			si = round(wind['speed'] * 0.621371192)
+			wenc += "s%03d" % si
+		else:
+			wenc += "s..."
+		if 'gust' in wind:
+			si = round(wind['gust'] * 0.621371192)
+			wenc += "g%03d" % si
+		else:
+			wenc += "g..."
+
+		# Temperature
+		if 'temperature' in w:
+			wenc += "t%03d" % round(w['temperature'] / (float(5)/9) + 32)
+		else:
+			wenc += "t..."
+
+		# Humidity
+		if 'humidity' in w:
+			h = w['humidity']
+			if h == 0: h = 1
+			if h == 100: h = 0
+			wenc += "h%02d" % h
+		else:
+			wenc += "h.."
+
+		# Atmospheric pressure
+		if 'pressure' in w:
+			wenc += "b%04d" % round(w['pressure'] * 10)
+
+		payload = "_%sPyMM" % wenc
 		return "%s>APRS,TCPIP*:%s" % (callsign, payload)
 	except:
 		return None
