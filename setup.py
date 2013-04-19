a81255c867956f544ee5e3dcae22688ded4ac595
Dominik Heidler <dheidler@gmail.com>
2013-04-19 11:31:32 +0200

    overwrite config location from commandline

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
 	name='pymultimonaprs',
-	version='0.8.0',
+	version='0.8.1',
 	license='GPL',
 	description='HF2APRS-IG Gateway',
 	author='Dominik Heidler',
