dd54dc7750c41425203789c79cdbd1ef47b3869a
Dominik Heidler <dominik@heidler.eu>
2014-10-30 18:47:29 +0100

    Stabilize network connection
    - Use blocking socket while writing
    - Use non-blocking socket while reading
    - Catch all socket exceptions and reconnect
    - Auth-Reconnect until success

--- pymultimonaprs/gate.py
+++ pymultimonaprs/gate.py
@@ -6,6 +6,8 @@ import Queue
 import pkg_resources
 import sys
 import logging
+import select
+from time import sleep
 
 class IGate:
 	def __init__(self, callsign, passcode, gateway):
@@ -27,30 +29,37 @@ class IGate:
 		self._disconnect()
 
 	def _connect(self):
-		# Connect
-		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		ip = socket.gethostbyname(self.server)
-		self.log.info("connecting... %s:%i" % (ip, self.port))
-		self.socket.connect((ip, self.port))
-		self.log.info("connected")
+		connected = False
+		while not connected:
+			try:
+				# Connect
+				self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				ip = socket.gethostbyname(self.server)
+				self.log.info("connecting... %s:%i" % (ip, self.port))
+				self.socket.connect((ip, self.port))
+				self.log.info("connected")
 
-		server_hello = self.socket.recv(1024)
-		self.log.info(server_hello.strip(" \r\n"))
+				server_hello = self.socket.recv(1024)
+				self.log.info(server_hello.strip(" \r\n"))
 
-		# Try to get my version
-		try:
-			version = pkg_resources.get_distribution("pymultimonaprs").version
-		except:
-			version = 'GIT'
+				# Try to get my version
+				try:
+					version = pkg_resources.get_distribution("pymultimonaprs").version
+				except:
+					version = 'GIT'
 
-		# Login
-		self.log.info("login %s (PyMultimonAPRS %s)" % (self.callsign, version))
-		self.socket.send("user %s pass %s vers PyMultimonAPRS %s filter r/38/-171/1\r\n" % (self.callsign, self.passcode, version))
+				# Login
+				self.log.info("login %s (PyMultimonAPRS %s)" % (self.callsign, version))
+				self.socket.send("user %s pass %s vers PyMultimonAPRS %s filter r/38/-171/1\r\n" %
+						(self.callsign, self.passcode, version))
 
-		server_return = self.socket.recv(1024)
-		self.log.info(server_return.strip(" \r\n"))
+				server_return = self.socket.recv(1024)
+				self.log.info(server_return.strip(" \r\n"))
 
-		self.socket.setblocking(0)
+				connected = True
+			except socket.error as e:
+				self.log.warn("Error when connecting to server: '%s'" % str(e))
+				sleep(1)
 
 	def _disconnect(self):
 		try:
@@ -58,8 +67,12 @@ class IGate:
 		except:
 			pass
 
+			# wait 10sec for queue slot, then drop the data
 	def send(self, frame):
-		self._sending_queue.put(frame)
+		try:
+			self._sending_queue.put(frame, True, 10)
+		except Queue.Full as e:
+			self.log.warn("Lost TX data (queue full): '%s'" % frame.export(False))
 
 	def _socket_worker(self):
 		"""
@@ -67,21 +80,37 @@ class IGate:
 		"""
 		while self._running:
 			try:
-				frame = self._sending_queue.get(True, 1)
-				self.log.debug("sending: %s" % frame.export(False))
-				self.socket.send("%s\r\n" % frame.export())
-			except Queue.Empty:
-				pass
-			except socket.error, e:
-				if e.errno == 32:
-					# socket disconnected
-					self.log.warn("socket dead")
-					self._connect()
-			# read from socket to prevent buffer fillup
-			try:
-				self.socket.recv(40960)
-			except socket.error, e:
-				if e.errno == 11:
-					# buffer empty
+				try:
+					# wait max 1sec for new data
+					frame = self._sending_queue.get(True, 1)
+					self.log.debug("sending: %s" % frame.export(False))
+					raw_frame = "%s\r\n" % frame.export()
+					totalsent = 0
+					while totalsent < len(raw_frame):
+						sent = self.socket.send(raw_frame[totalsent:])
+						if sent == 0:
+							raise socket.error(0, "Failed to send data - number of sent bytes: 0")
+						totalsent += sent
+				except Queue.Empty:
 					pass
+
+				# (try to) read from socket to prevent buffer fillup
+				self.socket.setblocking(0)
+				try:
+					res = self.socket.recv(40960)
+				except socket.error as e:
+					if not e.errno == 11:
+						# if the error is other than 'rx queue empty'
+						raise
+				self.socket.setblocking(1)
+			except socket.error as e:
+				# possible errors on io:
+				# [Errno  11] Buffer is empty (maybe not when using blocking sockets)
+				# [Errno  32] Broken Pipe
+				# [Errno 104] Connection reset by peer
+				# [Errno 110] Connection time out
+				self.log.warn("Connection issue: '%s'" % str(e))
+				sleep(1)
+				# try to reconnect
+				self._connect()
 		self.log.debug("sending thread exit")
--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from distutils.core import setup
 
 setup(
 	name='pymultimonaprs',
-	version='0.9.0',
+	version='1.0.0',
 	license='GPL',
-	description='HF2APRS-IG Gateway',
+	description='RF2APRS-IG Gateway',
 	author='Dominik Heidler',
-	author_email='dheidler@gmail.com',
+	author_email='dominik@heidler.eu',
 	url='http://github.com/asdil12/pymultimonaprs',
 	packages=['pymultimonaprs'],
 	scripts=['bin/pymultimonaprs'],
