c03329bb33b8fa7a959ab2ee71954987ba5b64cc
Dominik Heidler <dominik@heidler.eu>
2014-10-30 21:29:24 +0100

    don't overwrite existing config file

--- setup.py
+++ setup.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python2
 
 from distutils.core import setup
+from distutils.command.install import install
+
+class post_install(install):
+	def run(self):
+		install.run(self)
+		# install config file
+		print ""
+		import os
+		import shutil
+		cd = os.path.dirname(os.path.realpath(__file__))
+		src = os.path.join(cd, "pymultimonaprs.json")
+		if os.path.isfile("/etc/pymultimonaprs.json"):
+			print "Warning: /etc/pymultimonaprs.json already exists! Saved new config file to /etc/pymultimonaprs.json.new"
+			shutil.copyfile(src, "/etc/pymultimonaprs.json.new")
+		else:
+			print "Installing config file to /etc/pymultimonaprs.json"
+			shutil.copyfile(src, "/etc/pymultimonaprs.json")
+
+
 
 setup(
 	name='pymultimonaprs',
@@ -13,7 +32,8 @@ setup(
 	packages=['pymultimonaprs'],
 	scripts=['bin/pymultimonaprs'],
 	data_files=[
-		('/etc', ['pymultimonaprs.json']),
+		#('/etc', ['pymultimonaprs.json']),
 		('/usr/lib/systemd/system', ['pymultimonaprs.service']),
 	],
+	cmdclass={'install': post_install},
 )
