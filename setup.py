2383f6eaf511b365ab0de6b1b4df2b0692a7d065
Dominik Heidler <dominik@heidler.eu>
2015-08-27 18:59:06 +0200

    version 1.1.1

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ class post_install(install):
 
 setup(
 	name='pymultimonaprs',
-	version='1.1.0',
+	version='1.1.1',
 	license='GPL',
 	description='RF2APRS-IG Gateway',
 	author='Dominik Heidler',
