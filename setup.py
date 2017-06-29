5956a2d1da7ba48a44903564a1ec742b74c729fc
Marco kubon <dl1mx@gmx.de>
2017-06-29 22:49:10 +0200

    Update setup.py
    new version number

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ class post_install(install):
 
 setup(
 	name='pymultimonaprs',
-	version='1.2.0',
+	version='1.3.0',
 	license='GPL',
 	description='RF2APRS-IG Gateway',
 	author='Dominik Heidler',
